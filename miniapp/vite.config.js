15a961f112e5f0f3e12dd8d98f57f324cee63603
Dinechko <m@dichenko.ru>
2026-03-26 08:24:48 +0300

    Allow liven8n host in Vite

--- miniapp/vite.config.js
+++ miniapp/vite.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    allowedHosts: ['poverka-test-app.liven8n.site']
   }
 });
